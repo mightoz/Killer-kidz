9114247accb17989a0b3f75692c1a4aafdec6f83
MarieKlevedal <gusklevma@student.gu.se>
2015-04-26 21:17:49 +0200

    First version controller
    First version of GameController and TimeController finished

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,9 +1,10 @@
 package controller;
 
-/**
- * Created by Oscar on 24/04/15.
- */
-public class GameController implements KeyListener {
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+public class GameController implements KeyListener{
+    
     private Model model;
     
     private final int P1_UP = KeyEvent.VK_UP;
@@ -12,7 +13,43 @@ public class GameController implements KeyListener {
     private final int P1_RIGHT = KeyEvent.VK_RIGHT;
     private final int P1_THROW = KeyEvent.VK_SPACE;
 
-    public Controller(Model model) {
+    public GameController(Model model) {
         this.model = model;
+        TimeController tc = new TimeController(model);
     }
+
+    public void keyPressed(KeyEvent e) {
+    	switch (e.getKeyCode()) {
+    	case P1_UP:
+    		model.movePlayer(1, Direction.UP);
+    		break;
+    	case P1_DOWN:
+        	model.movePlayer(1, Direction.DOWN);
+    		break;
+    	case P1_LEFT:
+        	model.movePlayer(1, Direction.LEFT);
+    		break;
+    	case P1_RIGHT:
+        	model.movePlayer(1, Direction.RIGHT);
+    		break;
+    	default:
+    		// do nothing
+    	}
+    }
+    
+    public void	keyReleased(KeyEvent e) {
+    	switch (e.getKeyCode()) {
+    	case P1_THROW:
+    		model.throwCandy();
+    		break;
+    	// TODO: case changeCandy
+    	default:
+    		// do nothing
+    	}
+    }
+    
+    public void keyTyped(KeyEvent e) {
+    	// do nothing
+    }
+
 }
