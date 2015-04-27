3cdb64c61d9ec43de12b0731d7a465d18fd59a47
Mightoz <oscar.beronius@gmail.com>
2015-04-27 15:19:45 +0200

    player can now move

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -5,4 +5,5 @@ package model;
  */
 public class Model {
 
+
 }
--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -2,6 +2,7 @@ package model;
 
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import java.util.BitSet;
 
 /**
  * Created by Oscar on 24/04/15.
@@ -54,11 +55,54 @@ public class Player {
         return candyData;
     }
 
-    public void update(KeyEvent event){
-        
+    /**
+     * Updates the position of the player.
+     * @param directions array of booleans that tells what keys are pressed. Index 0 - 4 corresponds to the keys: left, up, right and down.
+     */
+    public void updatePos(boolean[] directions){
+
+        boolean leftKeyPressed = directions[0];
+        boolean upKeyPressed = directions[1];
+        boolean rightKeyPressed = directions[2];
+        boolean downKeyPressed = directions[3];
+        boolean leftAndRight = (!leftKeyPressed && !rightKeyPressed) || (leftKeyPressed && rightKeyPressed);
+        boolean upAndDown = (!upKeyPressed && !downKeyPressed) || (upKeyPressed && downKeyPressed);
+
+        //moves player up/down if up/down key is pressed and both left and right keys are pressed at the same time,
+        //or both left and right keys are not pressed at all.
+        if(leftAndRight && !upAndDown){
+            if(upKeyPressed)
+                y += delta;
+            else
+                y -= delta;
+        }
+        //moves player left/right if left/right key is pressed and both up and down keys are pressed at the same time,
+        //or both up and down keys are not pressed at all.
+        if(!leftAndRight && upAndDown){
+            if (rightKeyPressed)
+                x += delta;
+            else
+                x -= delta;
+        }
+        //moves player diagonally at the same speed as all other axis.
+        if(!leftAndRight && !upAndDown){
+            if(upKeyPressed && rightKeyPressed){
+                x += sqrt(2 * Math.pow(delta, 2));
+                y += sqrt(2 * Math.pow(delta, 2));
+            }else if(upKeyPressed){
+                x -= sqrt(2 * Math.pow(delta, 2));
+                y += sqrt(2 * Math.pow(delta, 2));
+            }else if(rightKeyPressed){
+                x += sqrt(2 * Math.pow(delta, 2));
+                y -= sqrt(2 * Math.pow(delta, 2));
+            }else{
+                x -= sqrt(2 * Math.pow(delta, 2));
+                y -= sqrt(2 * Math.pow(delta, 2));
+            }
+
+        }
 
     }
-
 
 
 
