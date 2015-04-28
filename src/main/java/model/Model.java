03feeecf5cb24748dd7d2f6c53a0d6407f08f013
MatildaHorppu <horppu@student.chalmers.se>
2015-04-28 17:41:03 +0200

    Added constructors and movePlayer() in Model

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -5,5 +5,32 @@ package model;
  */
 public class Model {
 
+    private Player player1;
+    private Player player2;
+
+    public Model(String playerName){
+        player1 = new Player(0, 0, playerName);
+    }
+
+    public Model(String player1Name, String player2Name){
+        player1 = new Player(0, 0, player1Name);
+        player2 = new Player(0, 0, player2Name);
+    }
+
+    public void movePlayer(boolean[] directions, int player){
+
+        if(player == 1) {
+            player1.updatePos(directions);
+        }else{
+            player2.updatePos(directions);
+        }
+
+
+
+    }
+
+    public void throwCandy(int player, int candyType){
+
+    }
 
 }
