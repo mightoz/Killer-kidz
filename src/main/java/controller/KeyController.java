90e6434715dbf04491eb87798c96df0a041f3bc6
MarieKlevedal <gusklevma@student.gu.se>
2015-04-30 11:16:52 +0200

    Changed name GameController -> KeyController

--- src/main/java/controller/KeyController.java
+++ src/main/java/controller/KeyController.java
@@ -4,7 +4,7 @@ import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import model.Model;
 
-public class GameController implements KeyListener{
+public class KeyController implements KeyListener{
     
     private Model model;
     private boolean[] p1Moves = {false, false, false, false};
@@ -15,9 +15,8 @@ public class GameController implements KeyListener{
     private final int[] P2_LURD = {KeyEvent.VK_A, KeyEvent.VK_W, KeyEvent.VK_D, KeyEvent.VK_S};
     private final int P2_THROW = KeyEvent.VK_CONTROL;
 
-    public GameController(Model model) {
+    public KeyController(Model model) {
         this.model = model;
-        new TimeController(model);
     }
 
 	// skicka array[4] med booleans LURD ist for direction
