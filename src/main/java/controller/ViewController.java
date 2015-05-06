67d63b38909a3e34538dfeb78eb8221e04bc55bb
MarieKlevedal <gusklevma@student.gu.se>
2015-05-06 16:40:55 +0200

    commented out things in ViewController that are not used

--- src/main/java/controller/ViewController.java
+++ src/main/java/controller/ViewController.java
@@ -8,27 +8,28 @@ import view.GameManager;
 
 public class ViewController implements ActionListener {
 
-	Model model;
-	GameManager view;
+	private final Model model;
+	private final GameManager view;
 	
 	public ViewController(Model model, GameManager view) {
 		this.model = model;
 		this.view = view;
+		new KeyController(model);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		switch (e.paramString()) {
-		case "newGame1p":
-			this.model = new Model("Player");
-			new KeyController(model);
-			break;
-		case "newGame2p":
-			this.model = new Model("Player1", "Player2");
-			new KeyController(model);
-			break;
-		default:
-			System.out.println(e.paramString() + ": listener not yet implemented");
-		}
+//		switch (e.paramString()) {
+//		case "newGame1p":
+//			this.model = new Model("Player");
+//			new KeyController(model);
+//			break;
+//		case "newGame2p":
+//			this.model = new Model("Player1", "Player2");
+//			new KeyController(model);
+//			break;
+//		default:
+//			System.out.println(e.paramString() + ": listener not yet implemented");
+//		}
 	}
 }
