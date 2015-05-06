2f554d8f7e6846d5ad87f8f07caf49b04c4657d2
DovahKiim <kibe.kim.berger@gmail.com>
2015-05-06 14:32:33 +0200

    updating ViewTwo

---
+++ src/main/java/view/mainMenuButtons/Button.java
@@ -0,0 +1,53 @@
+package view.mainMenuButtons;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+
+public class Button extends Game {
+
+	Stage stage;
+    TextButton button;
+    TextButtonStyle textButtonStyle;
+    BitmapFont font;
+    Skin skin;
+    TextureAtlas buttonAtlas;
+
+    @Override
+    public void create() {      
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        font = new BitmapFont();
+        skin = new Skin();
+        buttonAtlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack"));
+        skin.addRegions(buttonAtlas);
+        textButtonStyle = new TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.up = skin.getDrawable("up-button");
+        textButtonStyle.down = skin.getDrawable("down-button");
+        textButtonStyle.checked = skin.getDrawable("checked-button");
+        button = new TextButton("Button1", textButtonStyle);
+        stage.addActor(button);
+    }
+
+    @Override
+    public void render() {      
+        super.render();
+        stage.draw();
+    }
+    
+//    button.addListener(new ChangeListener() {
+//        @Override
+//        public void changed (ChangeEvent event, Actor actor) {
+//            System.out.println("Button Pressed");
+//        }
+//    });
+}
+
+
+
