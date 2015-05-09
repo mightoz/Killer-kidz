ed439a00f3840eb0e545523950b39eb1ac2eb9e3
MarieKlevedal <gusklevma@student.gu.se>
2015-05-09 15:04:50 +0200

    converted KeyController to libGDX code

--- src/main/java/controller/KeyController.java
+++ src/main/java/controller/KeyController.java
@@ -1,72 +1,75 @@
 package controller;
 
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
 import model.Model;
 
-public class KeyController implements KeyListener{
-    
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+
+public class KeyController extends InputListener {
     private Model model;
     private boolean[] p1Moves = {false, false, false, false};
     private boolean[] p2Moves = {false, false, false, false};
     
-    private final int nbrOfPlayers = 2;		// NOTE: have to change soon
-    private final int[] P1_LURD = {KeyEvent.VK_LEFT, KeyEvent.VK_UP, KeyEvent.VK_RIGHT, KeyEvent.VK_DOWN};
-    private final int P1_THROW = KeyEvent.VK_SPACE;
-    private final int[] P2_LURD = {KeyEvent.VK_A, KeyEvent.VK_W, KeyEvent.VK_D, KeyEvent.VK_S};
-    private final int P2_THROW = KeyEvent.VK_CONTROL;
-
+    private final int nbrOfPlayers = 1;		// NOTE: have to change soon
+    private final int[] P1_LURD = {Keys.LEFT, Keys.UP, Keys.RIGHT, Keys.DOWN};
+    private final int P1_THROW = Keys.SPACE; 
+    private final int[] P2_LURD = {Keys.A, Keys.W, Keys.D, Keys.S};
+    private final int P2_THROW = Keys.CONTROL_LEFT;
+  
     public KeyController(Model model) {
         this.model = model;
         new TimeController(model);
         //nbrOfPlayers = this.model.getNbrOfPlayers();
     }
 
-    public void keyPressed(KeyEvent e) {
-    	System.out.println("Test");
-    	int key = e.getKeyCode();
+	public boolean keyDown(InputEvent e, int keycode) {
     	for (int i = 0; i < 4; i++) {
-    		if (P1_LURD[i] == key) {
-    			p1Moves[i] = true;
-    			model.movePlayer(1, p1Moves);
-    		}
-    	}
-    	if (nbrOfPlayers == 2) {
+			if (P1_LURD[i] == keycode) {
+				p1Moves[i] = true;
+				model.movePlayer(1, p1Moves);
+				return true;
+			}
+		}
+		if (nbrOfPlayers == 2) {
 	    	for (int i = 0; i < 4; i++) {
-	    		if (P2_LURD[i] == key) {
+	    		if (P2_LURD[i] == keycode) {
 	    			p2Moves[i] = true;
 	    			model.movePlayer(2, p2Moves);
+	    			return true;
 	    		}
 	    	}
-    	}
-    }
-    
-    public void	keyReleased(KeyEvent e) {
-    	int key = e.getKeyCode();
+		}
+		return false;
+	}
+	
+	public boolean keyUp(InputEvent event, int keycode) {
     	for (int i = 0; i < 4; i++) {
-    		if (P1_LURD[i] == key) {
-    			p1Moves[i] = false;
-    			model.movePlayer(1, p1Moves);
-    		}
-    	}
-    	if (nbrOfPlayers == 2) {
+			if (P1_LURD[i] == keycode) {
+				p1Moves[i] = false;
+				model.movePlayer(1, p1Moves);
+				return true;
+			}
+		}
+		if (nbrOfPlayers == 2) {
 	    	for (int i = 0; i < 4; i++) {
-	    		if (P2_LURD[i] == key) {
+	    		if (P2_LURD[i] == keycode) {
 	    			p2Moves[i] = false;
 	    			model.movePlayer(2, p2Moves);
+	    			return true;
 	    		}
 	    	}
-    	}
-    	
-    	if (key == P1_THROW) {
-    		model.throwCandy(1);
-    	}
-    	if (nbrOfPlayers == 2 && key == P2_THROW) {
-    		model.throwCandy(2);
-    	}
-    }
-    
-    public void keyTyped(KeyEvent e) {
-    	// do nothing
-    }
+		}
+		
+		if (keycode == P1_THROW) {
+			model.throwCandy(1);
+			return true;
+		}
+		if (nbrOfPlayers == 2 && keycode == P2_THROW) {
+			model.throwCandy(2);
+			return true;
+		}
+		
+		return false;
+	}
 }
