edd8ef294346ffecbe615e6c093123cd21023d30
MarieKlevedal <gusklevma@student.gu.se>
2015-05-10 17:19:53 +0200

    kid able to move and notice if hit by candy

--- src/main/java/model/kids/Kid.java
+++ src/main/java/model/kids/Kid.java
@@ -1,36 +1,67 @@
 package model.kids;
 
 import model.Entity;
+import model.candymodels.Candy;
 
 public abstract class Kid extends Entity {
 
+	private static int idCounter = 0;
+	private final String ID;
+
+	protected double xHead, yHead;
+	protected int rHead;
+	protected int rBody;
+	
+	protected int vx, vy;
+	
 	public Kid(float x, float y) {
-		super(x, y);
-		// TODO Auto-generated constructor stub
+		// initiate position
+		super(x, y);			// body position
+		xHead = x;
+		yHead = y + rBody + rHead;
+		
+		ID = generateId();
 	}
 	
 	@Override
 	public String generateId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isExpired() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void update(int delta) {
-		// TODO Auto-generated method stub
-		
+		return "k" + (++idCounter);
 	}
 
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
+		return ID;
+	}
+	
+	// Default: no acceleration
+	@Override
+	public void update(int delta) {
+		xPos += vx*delta;
+		yPos += vy*delta;
+	}
+	
+	boolean hitByCandy(Candy c) {
+		int rCandy = 2; //c.getRadius();				// NOTE: change!
+		
+		if (pow2(xHead - c.getX()) + pow2(yHead - c.getY()) <= pow2(rHead + rCandy)) {
+			// Candy hit head
+			return true;
+		}
+		else if (pow2(xPos - c.getX()) + pow2(yPos - c.getY()) <= pow2(rBody + rCandy)) {
+			// Candy hit body
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private double pow2(double x) {
+		return x*x;
+	}
+	
+	@Override
+	public boolean isExpired() {
+		return false;
 	}
 
 }
--- src/main/java/model/kids/SimpleSam.java
+++ src/main/java/model/kids/SimpleSam.java
@@ -3,8 +3,12 @@ package model.kids;
 public class SimpleSam extends Kid {
 
 	public SimpleSam(float x, float y) {
-		super(x, y);
-		// TODO Auto-generated constructor stub
+		super(x, y);		
+		rHead = 5;		// NOTE: This may perhaps not work, as Kid needs rHead and rBody
+		rBody = 10;
+		
+		vx = -10;
+		vy = 0;
 	}
 
 }
