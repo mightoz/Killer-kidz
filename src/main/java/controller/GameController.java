d362fc3f93eb558afb0679a3de9aeeb63a06941c
MarieKlevedal <gusklevma@student.gu.se>
2015-05-19 19:16:55 +0200

    both players can change candy

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -9,20 +9,41 @@ import com.badlogic.gdx.InputAdapter;
 
 public class GameController extends InputAdapter {
     private Model model;
+    private final int nbrOfPlayers;    
+    
     private boolean[] p1Moves = {false, false, false, false};
     private boolean[] p2Moves = {false, false, false, false};
-    
-    private final int nbrOfPlayers;
-    private final int[] P1_LURD = {Keys.LEFT, Keys.UP, Keys.RIGHT, Keys.DOWN};
-    private final int P1_THROW = Keys.SPACE; 
+
+    private final int[] P1_LURD = new int[4];
     private final int[] P1_CANDY = {Keys.NUM_1, Keys.NUM_2, Keys.NUM_3, Keys.NUM_4};
-    private final int[] P2_LURD = {Keys.A, Keys.W, Keys.D, Keys.S};
-    private final int P2_THROW = Keys.CONTROL_LEFT;
+    private final int P1_THROW = Keys.SPACE;
+    private final int[] P2_LURD = new int [4];
+    private final int[] P2_CANDY = {Keys.NUMPAD_1, Keys.NUMPAD_2, Keys.NUMPAD_3, Keys.NUMPAD_4};
+    private final int P2_THROW = Keys.ENTER;
   
     public GameController(Model model, int nbrOfPlayers) {
-    	this.nbrOfPlayers = nbrOfPlayers;
         this.model = model;
-        System.out.println("GC constructor called");
+    	this.nbrOfPlayers = nbrOfPlayers;
+
+    	if (this.nbrOfPlayers == 1) {
+    	    P1_LURD[0] = Keys.LEFT;
+    	    P1_LURD[1] = Keys.UP;
+    	    P1_LURD[2] = Keys.RIGHT;
+    	    P1_LURD[3] = Keys.DOWN;
+    	}
+    	else if (this.nbrOfPlayers == 2) {
+    	    P1_LURD[0] = Keys.A;
+    	    P1_LURD[1] = Keys.W;
+    	    P1_LURD[2] = Keys.D;
+    	    P1_LURD[3] = Keys.S;
+    	    P2_LURD[0] = Keys.LEFT;
+    	    P2_LURD[1] = Keys.UP;
+    	    P2_LURD[2] = Keys.RIGHT;
+    	    P2_LURD[3] = Keys.DOWN;
+    	}
+    	else {
+    		System.out.println("Number of players must be 1 or 2");
+    	}
         
         Gdx.input.setInputProcessor(this);
         new TimeController(model);
@@ -78,6 +99,15 @@ public class GameController extends InputAdapter {
 				// model.changeCandy(1, i+1);
 				System.out.println("p1 changed candy to " + (i+1));
 				return true;
+			}
+		}
+		if (nbrOfPlayers == 2) {
+			for (int i = 0; i < 4; i++) {
+				if(P2_CANDY[i] == keycode) {
+					// model.changeCandy(2, i+1);
+					System.out.println("p2 changed candy to " + (i+1));
+					return true;
+				}
 			}
 		}
 		
