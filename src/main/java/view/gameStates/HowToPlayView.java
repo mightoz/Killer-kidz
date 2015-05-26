83d83f32d904ace03dbf031acbc9f0e95dd7f898
MarieKlevedal <gusklevma@student.gu.se>
2015-05-26 15:12:56 +0200

    Created HowToPlayView

---
+++ src/main/java/view/gameStates/HowToPlayView.java
@@ -0,0 +1,39 @@
+package view.gameStates;
+
+import view.GameManager;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * HowToPlayView
+ * @author  MarieKlevedal
+ * @version 1.0
+ */
+public class HowToPlayView implements Screen {
+
+	private GameManager gm;
+	
+	public HowToPlayView (GameManager gm) {
+		this.gm = gm;
+	}
+	
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void show() {}
+	@Override
+	public void resize(int width, int height) {}
+	@Override
+	public void pause() {}
+	@Override
+	public void resume() {}
+	@Override
+	public void hide() {}
+	@Override
+	public void dispose() {}
+
+}
