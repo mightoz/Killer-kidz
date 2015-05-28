fe2bb3e5f5e163068c0b3fbdfb7661c294e76013
MarieKlevedal <gusklevma@student.gu.se>
2015-05-28 18:05:46 +0200

    Added some tests to Sam

--- src/test/kidTests/SimpleSamTest.java
+++ src/test/kidTests/SimpleSamTest.java
@@ -1,6 +1,7 @@
 package kidTests;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import model.entity.kids.Kid;
@@ -10,55 +11,77 @@ public class SimpleSamTest {
 
 	@Test
 	public void testUpdate() {
-		// Makes sure that Sam keeps moving to the left,
-		// but keep the same yPos
-		Kid sam = new SimpleSam(500, 300);
+		SimpleSam sam = new SimpleSam(500, 300);
 		float firstX = sam.getX();
 		float firstY = sam.getY();
 		sam.update(0.1);
-		assertTrue(sam.getX() < firstX && sam.getY() == firstY);
+		assertTrue("Sam only moves to the left", sam.getX() < firstX);
+		assertEquals("Sam doesn't move vertically", firstY, sam.getY(), 1e-16);
 	}
 
 	@Test
 	public void testHitByCandy() {
-		fail("Not yet implemented");
+		SimpleSam sam = new SimpleSam(500, 300);
+		double hpRat = sam.getHpRatio();
+		sam.hitByCandy("JellyBean", 50);
+		assertTrue("Sam gets weaker by JellyBean", sam.getHpRatio() < hpRat);
+		// TODO: tests for other candies when I've decided exactly how Sam should behave
 	}
 
 	@Test
 	public void testSimpleSam() {
-		fail("Not yet implemented");
+		SimpleSam sam = new SimpleSam(500, 300);
+		assertNotNull("Sam is not null", sam);
+		assertEquals("Sam gets head radius 5", 5, sam.getHeadRadius(), 1e-16);
+		assertEquals("Sam gets radius 10", 10, sam.getRadius(), 1e-16);
+		assertEquals("Sam has full hp at start", 1, sam.getHpRatio(), 1e-16);
 	}
 
 	@Test
 	public void testGenerateId() {
-		fail("Not yet implemented");
+		SimpleSam sam1 = new SimpleSam(500, 300);
+		SimpleSam sam2 = new SimpleSam(500, 300);
+		assertNotEquals("Sams have unique ids", sam1.getId(), sam2.getId());
 	}
 
 	@Test
 	public void testIsExpired() {
-		fail("Not yet implemented");
+		SimpleSam sam = new SimpleSam(500, 300);
+		assertTrue("Sam is expored if entered store or dead", 
+				sam.isExpired() == sam.enteredStore() || sam.getHpRatio() == 0);
 	}
 
 	@Test
 	public void testGetId() {
-		fail("Not yet implemented");
+		SimpleSam sam = new SimpleSam(500, 300);
+		assertEquals("Sam id starts with k", "k", sam.getId().substring(0, 1));
 	}
 
 	@Test
 	public void testGetRadius() {
-		fail("Not yet implemented");
+		SimpleSam sam = new SimpleSam(500, 300);
+		float r1 = sam.getRadius();
+		sam.update(0.1);
+		sam.hitByCandy("JellyBean", 100);
+		assertEquals("Sam has constant radius", 10, r1, 1e-16);
 	}
 
 	@Test
 	public void testKid() {
-		fail("Not yet implemented");
+		Kid sam = new SimpleSam(500, 300);
+		assertNotNull("Sam not null");
+		assertEquals("Sam gets correct xPos", 500, sam.getX(), 1e-16);
+		assertEquals("Sam gets correct yPos", 300, sam.getY(), 1e-16);
+		assertEquals("Sams id starts with k", "k", sam.getId().substring(0, 1));
+		assertFalse("Sam isn't expired", sam.isExpired());
 	}
 
 	@Test
 	public void testGetHeadY() {
-		fail("Not yet implemented");
+		Kid sam = new SimpleSam(500, 300);
+		assertEquals("Sam has correct yPos", (float)(300+10+5), sam.getHeadY(), 1e-16);
 	}
-
+/*
 	@Test
 	public void testGetHeadRadius() {
 		fail("Not yet implemented");
@@ -113,5 +136,5 @@ public class SimpleSamTest {
 	public void testGetRightBoundary() {
 		fail("Not yet implemented");
 	}
-
+*/
 }
