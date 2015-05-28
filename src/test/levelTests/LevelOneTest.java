bab1141af39e98af76eb46fd8eb2ee00cc0b702b
MatildaHorppu <horppu@student.chalmers.se>
2015-05-28 21:22:26 +0200

    Added test files

---
+++ src/test/levelTests/LevelOneTest.java
@@ -0,0 +1,29 @@
+package model.levelmodels;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Matilda on 2015-05-28.
+ */
+public class LevelOneTest {
+
+    @org.junit.Test
+    public void testUpdateLevel() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testChangeWave() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testLevelDone() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testLevelFailed() throws Exception {
+
+    }
+}
