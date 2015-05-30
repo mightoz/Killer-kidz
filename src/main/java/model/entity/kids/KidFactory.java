5eff1bebd4d25274bddfee6feeb121980a1c0033
MarieKlevedal <gusklevma@student.gu.se>
2015-05-30 20:18:28 +0200

    All kids finished

--- src/main/java/model/entity/kids/KidFactory.java
+++ src/main/java/model/entity/kids/KidFactory.java
@@ -13,10 +13,10 @@ public abstract class KidFactory {
 		switch(type) {
 		case SIMPLE_SAM:
 			return new SimpleSam(x, y);
-		case SINESTER:
-			return new SinEster(x, y);
 		case DIZZY_DORIZ:
 			return new DizzyDoriz(x, y);
+		case SINESTER:
+			return new SinEster(x, y);
 		case GRANDALF_THE_GREY:
 			return new GrandalfTheGrey(x, y);
 		}
