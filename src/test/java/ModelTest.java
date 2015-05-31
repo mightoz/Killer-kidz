12634e3394243655e9a3a326c7a4f4226bdde44c
MatildaHorppu <horppu@student.chalmers.se>
2015-05-31 23:13:32 +0200

    Implemented ModelTest

--- src/test/java/ModelTest.java
+++ src/test/java/ModelTest.java
@@ -1,6 +1,10 @@
 import model.Model;
 import model.entity.Entity;
+import model.entity.players.Player;
+import model.levelmodels.Level;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -13,89 +17,61 @@ public class ModelTest {
     public void testMovePlayer(){
         Model model = new Model("Pelle", "Kalle", 600, 900);
         boolean dir[] = {false, true, false, false};
-        float initialY1;
-        float initialY2;
+        float initialY1 = 0;
+        float initialY2 = 0;
         for(int i = 0; i < model.getEntities().size(); i++){
-            initialY1 = model.getEntities().get(i).getY();
+            if(i == 0) {
+                initialY1 = model.getEntities().get(i).getY();
+            }else if(i == 1){
+                initialY2 = model.getEntities().get(i).getY();
+            }
         }
         model.movePlayer(1, dir);
         model.updateGame(1.0);
-        float newY1;
-
+        float newY1 = 0;
+        float newY2 = 0;
+        for(int i = 0; i < model.getEntities().size(); i++){
+            if(i == 0) {
+                newY1 = model.getEntities().get(i).getY();
+            }else if(i == 1){
+                newY2 = model.getEntities().get(i).getY();
+            }
+        }
+        assertTrue("movePlayer() does not update the right player", initialY1 < newY1 && initialY2 == newY2);
     }
 
-    @org.junit.Test
-    public void testChangeCandy() throws Exception {
-
+    @Test
+    public void testGetCurrentLevelNumber(){
+        Model model = new Model("Pelle", 600, 900);
+        int level = model.getCurrentLevelNumber();
+        assertTrue("getCurrentLevelNumber() returns wrong value", level == 1);
     }
 
-    @org.junit.Test
-    public void testThrowCandy() throws Exception {
-
+    @Test
+    public void testGetLevel(){
+        Model model = new Model("Pelle", 600, 900);
+        Level level = model.getLevel();
+        assertTrue("getLevel() does not return level", level != null);
     }
 
-    @org.junit.Test
-    public void testGetCurrentLevelNumber() throws Exception {
-
+    @Test
+    public void testGetWidth(){
+        Model model = new Model("Pelle", 600, 900);
+        float width = model.getWidth();
+        assertTrue("getWidth() returns wrong value", width == 600);
     }
 
-    @org.junit.Test
-    public void testGetLevel() throws Exception {
-
+    @Test
+    public void testGetHeight(){
+        Model model = new Model("Pelle", 600, 900);
+        float height = model.getHeight();
+        assertTrue("getWidth() returns wrong value", height == 900);
     }
 
-    @org.junit.Test
-    public void testStartLevel() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testUpdateGame() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testLevelCompleted() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testGetWidth() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testGetHeight() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testGetEntities() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testGetCandyShop() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testMoveMarkerInStore() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testChoose() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testGetStatusInShop() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testGetMoney() throws Exception {
-
+    @Test
+    public void testGetEntities(){
+        Model model = new Model("Pelle", 600, 900);
+        ArrayList<Entity> entities = model.getEntities();
+        assertTrue("getEntities() does not return the list", entities != null);
     }
 }
