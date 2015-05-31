6d3ebad0b7c1daa010963fc2af84152953086029
MatildaHorppu <horppu@student.chalmers.se>
2015-05-31 22:24:10 +0200

    Started implementing Modeltest

--- src/test/java/ModelTest.java
+++ src/test/java/ModelTest.java
@@ -1,3 +1,5 @@
+import model.Model;
+import model.entity.Entity;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,6 +11,16 @@ public class ModelTest {
 
     @Test
     public void testMovePlayer(){
+        Model model = new Model("Pelle", "Kalle", 600, 900);
+        boolean dir[] = {false, true, false, false};
+        float initialY1;
+        float initialY2;
+        for(int i = 0; i < model.getEntities().size(); i++){
+            initialY1 = model.getEntities().get(i).getY();
+        }
+        model.movePlayer(1, dir);
+        model.updateGame(1.0);
+        float newY1;
 
     }
 
