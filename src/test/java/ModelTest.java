baf4b634116105cb49397fc8e56de85465bc7b52
MatildaHorppu <horppu@student.chalmers.se>
2015-05-31 21:57:26 +0200

    Added ModelTest

---
+++ src/test/java/ModelTest.java
@@ -0,0 +1,89 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Matilda on 2015-05-31.
+ */
+public class ModelTest {
+
+    @Test
+    public void testMovePlayer(){
+
+    }
+
+    @org.junit.Test
+    public void testChangeCandy() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testThrowCandy() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetCurrentLevelNumber() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetLevel() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testStartLevel() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUpdateGame() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testLevelCompleted() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetWidth() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetHeight() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetEntities() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetCandyShop() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testMoveMarkerInStore() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testChoose() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetStatusInShop() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetMoney() throws Exception {
+
+    }
+}
